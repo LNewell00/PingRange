9352d810bc75c803a9b6f8ccb2d719b011484f62
lnewell00 <139590035+LNewell00@users.noreply.github.com>
2025-10-17 13:02:59 -0500

    Overthought the IP Increment

--- src/Main.java
+++ src/Main.java
@@ -7,48 +7,38 @@ public class Main {
 
         int[] startOctet = {192,168,0,0};
         int[] endOctet = {192,168, 8, 15};
-
-        main.walkIpRange(startOctet, endOctet);
-
     }
 
-    public void walkIpRange(int[] startOctet, int[] endOctet) throws IOException {
+    private int[] currentAddress = {0,0,0,0};
 
-        for(int i = startOctet[0]; i <= 255; i++) {
+    public synchronized void setCurrentAddress(int[] currentAddress) {
+        this.currentAddress = currentAddress;
+    }
 
-            if(i == endOctet[0] + 1) {
-                break;
-            }
+    public synchronized void setCurrentAddress(String currentAddress) {
+        String[] data = currentAddress.split("\\.");
+        this.currentAddress[0] = Integer.parseInt(data[0]);
+        this.currentAddress[1] = Integer.parseInt(data[1]);
+        this.currentAddress[2] = Integer.parseInt(data[2]);
+        this.currentAddress[3] = Integer.parseInt(data[3]);
+    }
 
-            for(int j = startOctet[1]; j <= 255; j++) {
+    public synchronized String getNextAddress() {
+        String next = String.format("%d.%d.%d.%d", currentAddress[0], currentAddress[1], currentAddress[2], currentAddress[3]);
 
-                if(j == endOctet[1] + 1 && i == endOctet[0]) {
-                    break;
-                }
-
-                for(int k = startOctet[2]; k <= 255; k++) {
-
-                    if(k == endOctet[2] + 1 && j == endOctet[1]) {
-                        break;
-                    }
-
-                    for(int l = startOctet[3]; l <= 255; l++) {
-
-                        if(l == endOctet[3] + 1 && k == endOctet[2]) {
-                            break;
-                        }
-
-                        String address = String.format("%d.%d.%d.%d", i, j, k, l);
-                        InetAddress inetAddress = InetAddress.getByName(address);
-                        System.out.printf("%s reachable: %s\n", address, inetAddress.isReachable(5));
-
-                        if(l == 1 && !inetAddress.isReachable(5)) {
-                            break;
-                        }
-                    }
-                }
+        currentAddress[3]++;
+        for (int i = 3; i >= 0; i--) {
+            if (currentAddress[i] > 255) {
+                currentAddress[i] = 0;
+                if (i > 0)
+                    currentAddress[i - 1]++;
             }
         }
 
+        if (currentAddress[0] > 255)
+            return null;
+
+
+        return next;
     }
 }
